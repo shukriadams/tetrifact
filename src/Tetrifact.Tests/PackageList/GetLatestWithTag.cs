1b5dcf73a70e200a651b1d84a5fe2e60aa7233bc
Shukri Adams <shukri.adams@gmail.com>
2019-05-12 17:51:30 +0200

    added popular tags test

--- src/Tetrifact.Tests/PackageList/GetLatestWithTag.cs
+++ src/Tetrifact.Tests/PackageList/GetLatestWithTag.cs
@@ -12,7 +12,7 @@ namespace Tetrifact.Tests.PackageList
         [Fact]
         public void BasicList()
         {
-            // tag list work by reading manifest json files on system. Create two manifests, all we need are dates on them
+            // list works by reading manifest json files on system. Create two manifests. All we need are dates on them.
             Directory.CreateDirectory(Path.Combine(Settings.PackagePath, "package2002"));
             Directory.CreateDirectory(Path.Combine(Settings.PackagePath, "package2001"));
             File.WriteAllText(Path.Combine(Settings.PackagePath, "package2002", "manifest.json"), JsonConvert.SerializeObject(new Manifest() { Tags = new HashSet<string>{ "tag" }, CreatedUtc = DateTime.Parse("2002/1/1") }));
