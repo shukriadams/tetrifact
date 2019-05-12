056272c15bc1c89f497c8e9795c48de38ca85f76
Shukri Adams <shukri.adams@gmail.com>
2019-05-13 00:05:02 +0200

    added memorycache to packagelist unit tests.

--- src/Tetrifact.Tests/PackageList/Base.cs
+++ src/Tetrifact.Tests/PackageList/Base.cs
@@ -1,14 +1,29 @@
-﻿using Tetrifact.Core;
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using Tetrifact.Core;
 
 namespace Tetrifact.Tests.PackageList
 {
-    public class Base : FileSystemBase
+    public class Base : FileSystemBase, IDisposable
     {
         protected IPackageList PackageList { get; private set; }
 
+        private IMemoryCache _memoryCache;
+
         public Base()
         {
-            this.PackageList = new Core.PackageList(null, Settings, new TestLogger<IPackageList>());
+            ServiceCollection services = new ServiceCollection();
+            services.AddMemoryCache();
+            ServiceProvider serviceProvider = services.BuildServiceProvider();
+            _memoryCache = serviceProvider.GetService<IMemoryCache>();
+
+            this.PackageList = new Core.PackageList(_memoryCache, Settings, new TestLogger<IPackageList>());
+        }
+
+        public void Dispose()
+        {
+            _memoryCache.Dispose();
         }
     }
 }
