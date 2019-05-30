7300b2f4438fef5d64ce670c0dd7a9928a7d407d
Shukri Adams <shukri.adams@gmail.com>
2019-05-30 13:51:11 +0200

    fixed typo in namespace

--- src/Tetrifact.Tests/TagsService/AddTag.cs
+++ src/Tetrifact.Tests/TagsService/AddTag.cs
@@ -3,7 +3,7 @@ using Tetrifact.Core;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Tetrifact.Tests.TagsServoce
+namespace Tetrifact.Tests.TagsService
 {
     public class AddTag : Base 
     {
