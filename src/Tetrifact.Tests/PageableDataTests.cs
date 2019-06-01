69cbb62b563d94b1e0955ab52932b5f1fc29a664
Shukri Adams <shukri.adams@gmail.com>
2019-06-01 10:42:53 +0200

    pageable data tests
    also added vscode settings

---
+++ src/Tetrifact.Tests/PageableDataTests.cs
@@ -0,0 +1,18 @@
+using Tetrifact.Core;
+using Xunit;
+
+namespace Tetrifact.Tests
+{
+    public class PageableDataTests
+    {
+        /// <summary>
+        /// Coverage compliance tests, ensures that modulus condition in PageableData ctor is reached.
+        /// </summary>
+        [Fact]
+        public void Modulus()
+        {
+            string[] items = new string[]{"1", "2", "3"};
+            new PageableData<string>(items, 0, 2, 3);
+        }
+    }
+}
