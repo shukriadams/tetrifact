d0cbcb1b5720c221b5557534ee34e5c9804b9bfb
Shukri Adams <shukri.adams@gmail.com>
2019-12-25 16:03:14 +0100

    fixed .netcore mvc refs

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -7,7 +7,7 @@ wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-pr
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.2 -y
+sudo apt-get install dotnet-sdk-3.1 -y
 
 # altecover report generator
 dotnet tool install --global dotnet-reportgenerator-globaltool --version 4.1.5
