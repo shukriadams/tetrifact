fd77c523b30c1c1832c64096f93166f7219a4a32
Shukri Adams <shukri@playdead.com>
2021-08-02 15:45:12 +0200

    upgraded vagrant
    bionic
    dotnet 3.1

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,11 +3,11 @@
 sudo apt-get update
 
 # dotnetcore
-wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get install apt-transport-https
 sudo apt-get update
-sudo apt-get install dotnet-sdk-2.2 -y
+sudo apt-get install dotnet-sdk-3.1 -y
 
 # altecover report generator
 dotnet tool install --global dotnet-reportgenerator-globaltool --version 4.1.5
