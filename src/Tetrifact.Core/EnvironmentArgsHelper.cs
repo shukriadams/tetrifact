4ea0006a08857e67519bf0f0540a42421511a9ec
Shukri Adams <shukri.adams@gmail.com>
2022-04-05 13:51:18 +0200

    added https + http2 support

---
+++ src/Tetrifact.Core/EnvironmentArgsHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Tetrifact.Core
+{
+    public class EnvironmentArgsHelper
+    {
+        public static bool GetAsBool(string name)
+        {
+            string value = Environment.GetEnvironmentVariable(name);
+            if (value == null)
+                value = string.Empty;
+
+            value = value.ToLower();
+
+            return value == "true" || value == "1";
+        }
+
+        public static int GetAsInt(string name, int defaultValue)
+        {
+            string value = Environment.GetEnvironmentVariable(name);
+            int val;
+            if (!int.TryParse(Environment.GetEnvironmentVariable(name), out val)){
+                Console.WriteLine($"Environment variable {value} expected to be integer, but could not be parsed, falling back to default {defaultValue}");
+                return defaultValue;
+            }
+
+            return val;
+        }
+    }
+}
--- src/Tetrifact.Web/Program.cs
+++ src/Tetrifact.Web/Program.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Hosting;
 using System;
 using Microsoft.Extensions.Logging;
+using System.Net;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Tetrifact.Core;
 
 namespace Tetrifact.Web
 {
@@ -23,11 +26,25 @@ namespace Tetrifact.Web
                 
 
             bool isIIS = Environment.GetEnvironmentVariable("IS_IIS") == "true";
+            bool useHTTPS = EnvironmentArgsHelper.GetAsBool("TETRIFACT_USE_HTTPS");
+            string httpsCertPath = Environment.GetEnvironmentVariable("TETRIFACT_HTTPS_CERT_PATH");
+            int port = EnvironmentArgsHelper.GetAsInt("TETRIFACT_PORT", 5001);
+            if (useHTTPS && string.IsNullOrEmpty(httpsCertPath)){
+                Console.WriteLine("TETRIFACT_HTTPS_CERT_PATH env var not set, cannot use HTTPS");
+                useHTTPS = false;
+            }
 
             if (!isIIS)
             {
                 builder.UseKestrel(options =>
                 {
+                    options.Listen(IPAddress.Any, port, listenOptions =>
+                    {
+                        listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                        if (useHTTPS)
+                            listenOptions.UseHttps(httpsCertPath);
+                    });
+
                     // SECURITY WARNING : the limit on attachment part size is removed to support large builds. 
                     options.Limits.MaxRequestBodySize = long.MaxValue;
                     options.Limits.MaxRequestBufferSize = long.MaxValue;
