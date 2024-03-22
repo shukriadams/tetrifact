d8fcb0bbfc9aee48d0609e31416db8f70aa4b168
Shukri Adams <shukri.adams@gmail.com>
2024-03-22 14:35:11 +0100

    added cover bat script

---
+++ src/cover.bat
@@ -0,0 +1,3 @@
+dotnet test /p:AltCover=true
+reportgenerator -reports:./Tetrifact.Tests/coverage.xml -targetdir:./Tetrifact.Tests/coverage -assemblyfilters:+Tetrifact.*;-Tetrifact.Tests -classfilters:-Tetrifact.Core.ThreadDefault;-Tetrifact.Web.DaemonProcessRunner;-Tetrifact.Web.Pager;-Tetrifact.Web.Program;-Tetrifact.Web.Startup;-Tetrifact.Web.ReadLevel;-Tetrifact.Web.WriteLevel;-*f__*
+start Tetrifact.Tests\coverage\index.html
