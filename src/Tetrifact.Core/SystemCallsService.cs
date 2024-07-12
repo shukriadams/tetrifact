9672ee5eacc3f8f51e7d54caca9843315988bdcd
Shukri Adams <shukri.adams@gmail.com>
2024-07-12 12:01:57 +0200

    shell timeouts
    set shell timeouts to extremely high values on long calls

--- src/Tetrifact.Core/SystemCallsService.cs
+++ src/Tetrifact.Core/SystemCallsService.cs
@@ -25,7 +25,7 @@ namespace Tetrifact.Core
         public ShellResult GetRepoFilesCount()
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                return Shell.Run($"find {_settings.RepositoryPath} -type f | wc -l");
+                return Shell.Run($"find {_settings.RepositoryPath} -type f | wc -l", false, 3600000); // 1 hour timeout, we need to move this to settings
 
             // return 0 on non-linux systems
             return new ShellResult(0, new List<string> { "0" }, new List<string>() );
@@ -34,7 +34,7 @@ namespace Tetrifact.Core
         public ShellResult GetRepoFilesSize()
         {
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-                return Shell.Run($"du -b --max-depth=0 {_settings.RepositoryPath}");
+                return Shell.Run($"du -b --max-depth=0 {_settings.RepositoryPath}", false, 3600000); // 1 hour timeout, we need to move this to settings
 
             // return 0 on non-linux systems
             return new ShellResult(0, new List<string> { $"0      {_settings.RepositoryPath}" }, new List<string>());
