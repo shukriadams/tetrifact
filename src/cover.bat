ed340f24a3a939b30d89c6927e2de3f6810d90d8
Shukri Adams <shukri.adams@gmail.com>
2026-03-06 16:01:08 +0100

    cover script cleanup
    removed duplicate cover scripts

--- src/cover.bat
+++ src/cover.bat
@@ -1,7 +1,18 @@
 :: Convenient single-click script to run cover. Works on Windows only.
 
+rmdir ./Tetrifact.Tests/coverage /s /q
+
+:: run test
 dotnet test /p:AltCover=true
 
-reportgenerator -reports:./Tetrifact.Tests/coverage.xml -targetdir:./Tetrifact.Tests/coverage -assemblyfilters:+Tetrifact.*;-Tetrifact.Tests -classfilters:-Tetrifact.Core.ThreadDefault;-Tetrifact.Web.DaemonProcessRunner;-Tetrifact.Web.Pager;-Tetrifact.Web.Program;-Tetrifact.Web.Startup;-Tetrifact.Web.ReadLevel;-Tetrifact.Web.WriteLevel;-*f__*
+:: generate cover
+reportgenerator ^
+    -reports:./Tetrifact.Tests/coverage.xml ^
+    -targetdir:./Tetrifact.Tests/coverage ^
+    -assemblyfilters:"+Tetrifact.*;-Tetrifact.Tests;-Tetrifact.Web.Views" ^
+    -classfilters:"-Tetrifact.Core.ThreadDefault;-Tetrifact.Web.DaemonProcessRunner;-Tetrifact.Web.Pager;-Tetrifact.Web.Program;-Tetrifact.Web.Startup;-Tetrifact.Web.ReadLevel;-Tetrifact.Web.WriteLevel;-*f__*"
 
-start Tetrifact.Tests\coverage\index.html
+:: open cover report in browser
+start "file:///%cd%/Tetrifact.Tests/coverage/index.html"
+
+pause
--- src/cover.sh
+++ src/cover.sh
@@ -1,11 +1,14 @@
-# Convenient single-click script to run cover. 
+#! /bin/sh
+
+# convenient single-click test + report batch file for local dev. Double-click in windows explorer to run.
+rm -rf ./Tetrifact.Tests/coverage
 
 dotnet test /p:AltCover=true
 
 reportgenerator \
     -reports:./Tetrifact.Tests/coverage.xml \
     -targetdir:./Tetrifact.Tests/coverage \
-    -assemblyfilters:"+Tetrifact.*;-Tetrifact.Tests" \
+    -assemblyfilters:"+Tetrifact.*;-Tetrifact.Tests;-Tetrifact.Web.Views;" \
     -classfilters:"-Tetrifact.Core.ThreadDefault;-Tetrifact.Web.DaemonProcessRunner;-Tetrifact.Web.Pager;-Tetrifact.Web.Program;-Tetrifact.Web.Startup;-Tetrifact.Web.ReadLevel;-Tetrifact.Web.WriteLevel;-*f__*"
 
 open Tetrifact.Tests/coverage/index.html
